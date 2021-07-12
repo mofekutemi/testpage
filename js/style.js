d779efa275a3a125cffa3d40904f774c31773c2c
krummitz <amidbidee@gmail.com>
2021-07-12 18:17:36 +0100

    made changes

---
+++ js/style.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("side-nav").style.width = "100%";
+}
+function closeNav() {
+    document.getElementById("side-nav").style.width = "0";
+    document.getElementById("main").style.marginleft = "0";
+}
